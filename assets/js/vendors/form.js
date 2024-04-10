daf79853cdf9de1dd018adfa31bdb16fd426fcff
Jonathan Gomez <tec-gomez.jonathan@hotmail.com>
2024-04-10 13:34:50 -0300

    Se agrega creacion de cards de forma dinamica en base a la respuesta del backend

--- assets/js/vendors/form.js
+++ assets/js/vendors/form.js
@@ -70,8 +70,11 @@ document.getElementById("hijos_si").addEventListener("click", function () {
       let cantidad = parseInt(this.value);
       let edadesHtml = "";
       for (let i = 0; i < cantidad; i++) {
-        edadesHtml +=
-          `<input class="form-control" type="number" id="edad_hijo_${i+1}" name="edad_hijo_${i+1}" placeholder="Edad Hijo ${i+1}" required>`;
+        edadesHtml += `<input class="form-control" type="number" id="edad_hijo_${
+          i + 1
+        }" name="edad_hijo_${i + 1}" placeholder="Edad Hijo ${
+          i + 1
+        }" required>`;
       }
       document.getElementById("edades_hijos").innerHTML = edadesHtml;
     });
@@ -81,8 +84,8 @@ document.getElementById("hijos_no").addEventListener("click", function () {
   document.getElementById("hijos_section").style.display = "none";
   document.getElementById("edades_hijos").innerHTML = "";
 });
-
-document.getElementById("cotizar").addEventListener("click", function (event) {
+const botonEnviar = document.getElementById("cotizar");
+botonEnviar.addEventListener("click", function (event) {
   event.preventDefault();
 
   // Crear el objeto JSON según el formato deseado
@@ -116,7 +119,6 @@ document.getElementById("cotizar").addEventListener("click", function (event) {
       i <= parseInt(document.getElementById("cantidad_hijos").value);
       i++
     ) {
-      console.log(document.getElementById(`edad_hijo_${i}`).value);
       jsonData.ageSons.push(
         parseInt(document.getElementById(`edad_hijo_${i}`).value)
       );
@@ -124,9 +126,8 @@ document.getElementById("cotizar").addEventListener("click", function (event) {
   } else if (opcionNo.checked) {
     jsonData.sons = false;
   }
-  console.log(jsonData);
   // Enviar los datos del formulario a la URL deseada
-  fetch("https://2be6-190-210-23-53.ngrok-free.app/quote/2", {
+  fetch("https://c2a6-190-210-23-53.ngrok-free.app/quote/2", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -140,7 +141,7 @@ document.getElementById("cotizar").addEventListener("click", function (event) {
       //return response.json();
       response.json().then((quotes) => {
         console.log(quotes);
-        const quotesContainer = document.getElementById("cotizaciones");
+        /*const quotesContainer = document.getElementById("cotizaciones");
         quotesContainer.innerHTML = "";
 
         let listNods = document.createElement("ul");
@@ -181,14 +182,262 @@ document.getElementById("cotizar").addEventListener("click", function (event) {
             listDs.appendChild(element);
           }
         }
-        quotesContainer.appendChild(listDs);
+        quotesContainer.appendChild(listDs); */
+        createCardQuote(quotes);
       });
     })
     .catch((error) => {
       console.error("Error al enviar los datos:");
       console.log(error.message);
     });
-})
+});
+
+function createCardQuote(quotes) {
+  const quotesContainer = document.getElementById("cotizaciones");
+  //quotesContainer.innerHTML = "";
+  let estadoCivil = document.getElementById("estado_civil").value;
+  let data = {
+    cobertura: "",
+    edad: document.getElementById("edad_titular").value,
+    hijos: "",
+    contratacion: document.getElementById("tipo_contratacion").value,
+  };
+  if (estadoCivil === "Soltero") {
+    data.cobertura = "Individual";
+  } else {
+    data.cobertura = "Matrimonio";
+  }
+  let hijos_si = document.getElementById("hijos_si");
+  let hijos_no = document.getElementById("hijos_no");
+  if (hijos_si.checked) {
+    data.hijos = "Con Hijos";
+  }
+  if (hijos_no.checked) {
+    data.hijos = "Sin Hijos";
+  }
+  for (let key in quotes.WithDs) {
+    let quoteNoDs = parseFloat(quotes.NoDs[key]).toFixed(2);
+    let quoteWithDs = parseFloat(quotes.WithDs[key]).toFixed(2);
+    let card = document.createElement("div");
+    card.classList.add("col-xl-4", "col-lg-5");
+    let descuentoTitular = "";
+    switch (data.contratacion) {
+      case "Recibo de Sueldo":
+        descuentoTitular = `
+      <span>Aportes Titular RelDep</span>
+      <span class="text-success fw-semibold"> -$${quotes.Discounts.Owner}</span>`;
+        break;
+      case "Monotributo":
+        descuentoTitular = `
+        <span>Aporte/Descuento Monotributo</span>
+        <span class="text-success fw-semibold"> -$${quotes.Discounts.Owner}</span>`;
+        break;
+    }
+    let cardHtml = "";
+    if (
+      estadoCivil === "Soltero" &&
+      parseInt(document.getElementById("edad_titular").value) <= 60
+    ) {
+      cardHtml = `
+      <div class="col-xl-4 col-lg-5">
+        <div class="card mt-4 mt-lg-0">
+          <div class="card-body">
+            <div class="mb-4 d-flex justify-content-between align-items-center">
+              <h4 class="mb-1">Cotización Plan</h4>
+            </div>
+            <div class="d-md-flex">
+              <div>
+                <img src="../assets/images/Zipper.PNG" alt="" class="+img-4by3-xl rounded" width="120px" />
+              </div>
+              <div class="ms-md-4 mt-2">
+                <h4 class="mb-1 text-primary-hover">${key}</h4>
+                  <h5>${data.cobertura}. ${data.edad}. ${data.hijos}</h5>
+              </div>
+            </div>
+          </div>
+          <div class="card-body border-top pt-2">
+            <ul class="list-group list-group-flush mb-0">
+              <li class="d-flex justify-content-between list-group-item px-0">
+                <span>Subtotal</span>
+                <span class="text-dark fw-semibold"><del>$${quoteNoDs}</del></span>
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                <span>Bonif. Af. online %${quotes.Discounts.Promo}</span>
+                <span class="text-success fw-semibold"> -$${parseFloat(
+                  quoteNoDs * (quotes.Discounts.Promo / 100)
+                ).toFixed(2)}</span>
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                ${descuentoTitular}
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0 pb-0">
+                <span class="fs-3 fw-semibold text-dark">Cuota Mensual</span>
+                <span class="fs-3 fw-semibold text-dark">$${quoteWithDs}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+    }
+
+    if (
+      estadoCivil === "Soltero" &&
+      parseInt(document.getElementById("edad_titular").value) > 60
+    ) {
+      cardHtml = `
+      <div class="col-xl-4 col-lg-5">
+        <div class="card mt-4 mt-lg-0">
+          <div class="card-body">
+            <div class="mb-4 d-flex justify-content-between align-items-center">
+              <h4 class="mb-1">Cotización Plan</h4>
+            </div>
+            <div class="d-md-flex">
+              <div>
+                <img src="../assets/images/Zipper.PNG" alt="" class="+img-4by3-xl rounded" width="120px" />
+              </div>
+              <div class="ms-md-4 mt-2">
+                <h4 class="mb-1 text-primary-hover">${key}</h4>
+                  <h5>${data.cobertura}. ${data.edad}. ${data.hijos}</h5>
+              </div>
+            </div>
+          </div>
+          <div class="card-body border-top pt-2">
+            <ul class="list-group list-group-flush mb-0">
+              <li class="d-flex justify-content-between list-group-item px-0">
+                <span>Subtotal</span>
+                <span class="text-dark fw-semibold"><del>$${quoteNoDs}</del></span>
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                ${descuentoTitular}
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0 pb-0">
+                <span class="fs-3 fw-semibold text-dark">Cuota Mensual</span>
+                <span class="fs-3 fw-semibold text-dark">$${quoteWithDs}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+    }
+    if (estadoCivil === "Casado" || estadoCivil === "Concubinato") {
+      if (parseInt(document.getElementById("edad_titular").value) <= 60) {
+        let descuentoEsposa = "";
+        if (
+          document.getElementById("tipo_contratacion_esposa").value ===
+          "Recibo de Sueldo"
+        ) {
+          descuentoEsposa = `<span>Aportes Esposa/Conyugue RelDep</span>
+        <span class="text-success fw-semibold"> -$${quotes.Discounts.Wife.toFixed(2)}</span>`;
+        } else if (
+          document.getElementById("tipo_contratacion_esposa").value ===
+          "Monotributo"
+        ) {
+          descuentoEsposa = `<span>Aporte/Descuento Monotributo Esposa/Conyugue</span>
+        <span class="text-success fw-semibold"> -$${quotes.Discounts.Wife.toFixed(2)}</span>`;
+        }
+
+        cardHtml = `
+      <div class="col-xl-4 col-lg-5">
+        <div class="card mt-4 mt-lg-0">
+          <div class="card-body">
+            <div class="mb-4 d-flex justify-content-between align-items-center">
+              <h4 class="mb-1">Cotización Plan</h4>
+            </div>
+            <div class="d-md-flex">
+              <div>
+                <img src="../assets/images/Zipper.PNG" alt="" class="+img-4by3-xl rounded" width="120px" />
+              </div>
+              <div class="ms-md-4 mt-2">
+                <h4 class="mb-1 text-primary-hover">${key}</h4>
+                  <h5>${data.cobertura}. ${data.edad}. ${data.hijos}</h5>
+              </div>
+            </div>
+          </div>
+          <div class="card-body border-top pt-2">
+            <ul class="list-group list-group-flush mb-0">
+              <li class="d-flex justify-content-between list-group-item px-0">
+                <span>Subtotal</span>
+                <span class="text-dark fw-semibold"><del>$${quoteNoDs}</del></span>
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                <span>Bonif. Af. online %${quotes.Discounts.Promo}</span>
+                <span class="text-success fw-semibold"> -$${parseFloat(
+                  quoteNoDs * (quotes.Discounts.Promo / 100)
+                ).toFixed(2)}</span>
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                ${descuentoTitular}
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                ${descuentoEsposa}
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0 pb-0">
+                <span class="fs-3 fw-semibold text-dark">Cuota Mensual</span>
+                <span class="fs-3 fw-semibold text-dark">$${quoteWithDs}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+      }
+      if (parseInt(document.getElementById("edad_titular").value) > 60) {
+        let descuentoEsposa = "";
+        if (
+          document.getElementById("tipo_contratacion_esposa").value ===
+          "Recibo de Sueldo"
+        ) {
+          descuentoEsposa = `<span>Aportes Esposa/Conyugue RelDep</span>
+        <span class="text-success fw-semibold"> -$${quotes.Discounts.Wife.toFixed(2)}</span>`;
+        } else if (
+          document.getElementById("tipo_contratacion_esposa").value ===
+          "Monotributo"
+        ) {
+          descuentoEsposa = `<span>Aporte/Descuento Monotributo Esposa/Conyugue</span>
+        <span class="text-success fw-semibold"> -$${quotes.Discounts.Wife.toFixed(2)}</span>`;
+        }
+        cardHtml = `
+      <div class="col-xl-4 col-lg-5">
+        <div class="card mt-4 mt-lg-0">
+          <div class="card-body">
+            <div class="mb-4 d-flex justify-content-between align-items-center">
+              <h4 class="mb-1">Cotización Plan</h4>
+            </div>
+            <div class="d-md-flex">
+              <div>
+                <img src="../assets/images/Zipper.PNG" alt="" class="+img-4by3-xl rounded" width="120px" />
+              </div>
+              <div class="ms-md-4 mt-2">
+                <h4 class="mb-1 text-primary-hover">${key}</h4>
+                  <h5>${data.cobertura}. ${data.edad}. ${data.hijos}</h5>
+              </div>
+            </div>
+          </div>
+          <div class="card-body border-top pt-2">
+            <ul class="list-group list-group-flush mb-0">
+              <li class="d-flex justify-content-between list-group-item px-0">
+                <span>Subtotal</span>
+                <span class="text-dark fw-semibold"><del>$${quoteNoDs}</del></span>
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                ${descuentoTitular}
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0">
+                ${descuentoEsposa}
+              </li>
+              <li class="d-flex justify-content-between list-group-item px-0 pb-0">
+                <span class="fs-3 fw-semibold text-dark">Cuota Mensual</span>
+                <span class="fs-3 fw-semibold text-dark">$${quoteWithDs}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>`;
+      }
+    }
+    card.innerHTML = cardHtml;
+    quotesContainer.appendChild(card);
+  }
+}
 
 /* document
   .getElementById("formulariooo")
