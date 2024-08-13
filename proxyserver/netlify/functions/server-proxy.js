318f595e4ebb0c86231a946b276e3b9f7ce67735
facufr12 <facufr2@hotmail.com>
2024-08-13 10:58:56 -0300

    cambio cards

--- proxyserver/netlify/functions/server-proxy.js
+++ proxyserver/netlify/functions/server-proxy.js
@@ -3,6 +3,7 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 const port = 3000; // Verifica si este es el puerto correcto
+
 // URL de tu Google Apps Script
 const apiUrl =
   "https://script.googleusercontent.com/a/macros/grupocober.online/echo?user_content_key=J5R3cd9wY7-9TpdNX8hPDVY-LfPU1beFfDso1v-PHvSOxOVHOiUL5BGGWP-i7fhdl8D_BNPYIYEJHzesiIXJqkBCUbtYatwCOJmA1Yb3SEsKFZqtv3DaNYcMrmhZHmUMi80zadyHLKAMvgmFF6Zo6ewm7a-wb37p24oBiCXIgg57e7UpQit3gzqLTyZZggD6ZwXMhLey5kacU0YuJvwA-9QNyZk34nLXOmjK0qsn17vY6-ZSd8LBcCMi9-kNHsG7aFx8hY91bYydJoCw8JvXDg&lib=MnJJWfZZA9dvbj3Wwchp_d2QyKQmgjrVn";
